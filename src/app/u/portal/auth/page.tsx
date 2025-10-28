56474bfd65d5ecb8fcbbbfd0d1cc35634ad19843
Web EGSPGOI <web@egspec.org>
2025-10-28 06:57:54 +0000

    ⨯ useSearchParams() should be wrapped in a suspense boundary at page "/u

--- src/app/u/portal/auth/page.tsx
+++ src/app/u/portal/auth/page.tsx
@@ -1,8 +1,16 @@
-
 "use client";
 
 import { LoginScreen } from "@/components/ui/login-1";
+import { Suspense } from "react";
+
+function LoginPageContent() {
+  return <LoginScreen />;
+}
 
 export default function LoginPage() {
-  return <LoginScreen />;
+  return (
+    <Suspense>
+      <LoginPageContent />
+    </Suspense>
+  );
 }
